3a7c0f957a28795ca31947c0ff938ea2271a5e9e
Alam <alam.15@outlook.com>
2022-04-13 21:48:41 -0400

    starter commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FinalProjectIDS309.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
