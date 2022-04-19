4c2730ca67b5616dfe2bea43a50c8ffa440339d2
Alam <alam.15@outlook.com>
2022-04-18 21:42:18 -0400

    move connection string to appsettings file

--- Context/DBContextConfig.cs
+++ Context/DBContextConfig.cs
@@ -3,14 +3,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FinalProjectIDS309.Context
 {
-    public class DBContextConfig : DbContext
-    {
-        public DbSet<CategoryModel> Categories { get; set; }
-        public DbSet<ItemModel> Items { get; set; }
+  public class DBContextConfig : DbContext
+  {
+    private IConfiguration _config { get; }
+    public DbSet<CategoryModel> Categories { get; set; }
+    public DbSet<ItemModel> Items { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
-        {
-            optionsBuilder.UseSqlServer(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=DbTodo;Integrated Security=True");
-        }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlServer(_config.GetConnectionString("DbConnection"));
     }
+
+    public DBContextConfig(IConfiguration config)
+    {
+      _config = config;
+    }
+  }
 }
