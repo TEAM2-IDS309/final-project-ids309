72e32dfec40ccd3f264ceb6b3773335c54af4952
Alam <alam.15@outlook.com>
2022-04-20 21:54:51 -0400

    add team page

---
+++ Controllers/TeamController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProjectIDS309.Controllers
+{
+  public class TeamController : Controller
+  {
+    public ViewResult Index()
+    {
+      return View();
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -15,9 +15,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Categories}/{action=Index}/{id?}");
 
 app.Run();
