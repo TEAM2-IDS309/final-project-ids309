d795ee775759b58e9fffacfaafeb84c99f9a45c0
Alam <alam.15@outlook.com>
2022-04-24 11:55:31 -0400

    add app controller

---
+++ Controllers/AppController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FinalProjectIDS309.Models;
+
+namespace FinalProjectIDS309.Controllers;
+
+public class AppController : Controller
+{
+  private readonly ILogger<AppController> _logger;
+
+  public AppController(ILogger<AppController> logger)
+  {
+    _logger = logger;
+  }
+
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
+}
